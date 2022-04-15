9335a09a788833de22a1c8885b4ffe415d228d32
zippelgazz <46674488+indexindex@users.noreply.github.com>
2022-04-15 16:52:28 +0300

    header styles
    added - header styles with animation

---
+++ docs/main.baf25138d4d0835917a3.hot-update.js
@@ -0,0 +1,32 @@
+"use strict";
+self["webpackHotUpdate"]("main",{
+
+/***/ "./sass/main.scss":
+/*!************************!*\
+  !*** ./sass/main.scss ***!
+  \************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+    if(true) {
+      // 1650030699498
+      var cssReload = __webpack_require__(/*! ../../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.id, {"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "b314c703e914a941d840"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.baf25138d4d0835917a3.hot-update.js.map
